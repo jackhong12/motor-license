45b1be9e2b6d94af43c98dde13027ec536c3488a
jack <jackhuang1205@gmail.com>
2025-05-11 00:03:31 +0800

    Send mails with markdown format

--- sendmail.py
+++ sendmail.py
@@ -1,30 +1,60 @@
 import smtplib
+import markdown
 import private 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def sendMails (body, subject="機車有名額時段"):
-    # Email configuration
-    sender_email = private.EMAIL_USER 
-    password = private.EMAIL_PASS 
-    
-    for receiver_email in private.EMAIL_RECV.split(";"):
+class MailHandler:
+    def __init__ (self):
+        self.content = ""
 
-        # Compose the email
-        message = MIMEMultipart()
-        message["From"] = sender_email
-        message["To"] = receiver_email
-        message["Subject"] = subject
+    def bold (self, msg):
+        self.content += f"**{msg}**"
 
-        # Add body to email
-        message.attach(MIMEText(body, "plain"))
+    def text (self, msg):
+        self.content += f"{msg}"
 
-        # Send the email
-        try:
-            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-                server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, message.as_string())
-            print(f"Email sent to {receiver_email} successfully!")
-        except Exception as e:
-            print(f"Error: {e}")
+    def textln (self, msg):
+        self.content += f"{msg}\n"
 
+    def newParagraph (self):
+        self.content += "---\n"
+
+    def send (self):
+        html = markdown.markdown(self.content)
+        self.send_impl(html)
+
+    def send_impl (self, txt, subject="機車有名額時段"):
+        # Email configuration
+        sender_email = private.EMAIL_USER 
+        password = private.EMAIL_PASS 
+        
+        for receiver_email in private.EMAIL_RECV.split(";"):
+
+            # Compose the email
+            message = MIMEMultipart()
+            message["From"] = sender_email
+            message["To"] = receiver_email
+            message["Subject"] = subject
+
+            # Add body to email
+            message.attach(MIMEText(txt, "html"))
+
+            # Send the email
+            try:
+                with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                    server.login(sender_email, password)
+                    server.sendmail(sender_email, receiver_email, message.as_string())
+                print(f"Email sent to {receiver_email} successfully!")
+            except Exception as e:
+                print(f"Error: {e}")
+
+if __name__ == '__main__':
+    mail = MailHandler()
+    mail.textln("## Hello")
+    mail.textln("- Hello")
+    mail.textln("- Hello")
+    mail.newParagraph()
+    mail.textln("- Hello")
+    mail.newParagraph()
+    mail.send()
